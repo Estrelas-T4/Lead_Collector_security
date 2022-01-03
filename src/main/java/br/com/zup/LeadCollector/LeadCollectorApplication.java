5941a84281bd767a20bc69ef5f04a2c19180e827
Vinícius <vinicius.raymundo@zup.com.br>
2022-01-03 13:56:53 -0300

    Inciar projeto

---
+++ src/main/java/br/com/zup/LeadCollector/LeadCollectorApplication.java
@@ -0,0 +1,13 @@
+package br.com.zup.LeadCollector;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LeadCollectorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LeadCollectorApplication.class, args);
+	}
+
+}
