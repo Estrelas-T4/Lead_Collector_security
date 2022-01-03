dbadb7eec153341db57872c51b349938092492ab
Vinícius <vinicius.raymundo@zup.com.br>
2022-01-03 14:05:40 -0300

    Criar service para usuario

---
+++ src/main/java/br/com/zup/LeadCollector/usuario/UsuarioService.java
@@ -0,0 +1,11 @@
+package br.com.zup.LeadCollector.usuario;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UsuarioService {
+    @Autowired
+    private UsuarioRepository usuarioRepository;
+
+}
