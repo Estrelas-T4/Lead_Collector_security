c1f7daeec4a992a894b362e7f9901e8b014a3838
Vinícius <vinicius.raymundo@zup.com.br>
2022-01-04 08:59:01 -0300

    Corrigir estrategia do gerador de valor para id do usuario

--- src/main/java/br/com/zup/LeadCollector/usuario/Usuario.java
+++ src/main/java/br/com/zup/LeadCollector/usuario/Usuario.java
@@ -7,7 +7,7 @@ import java.util.UUID;
 public class Usuario {
 
     @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @GeneratedValue(strategy = GenerationType.AUTO)
     private UUID id;
     @Column(nullable = false)
     private String email;
