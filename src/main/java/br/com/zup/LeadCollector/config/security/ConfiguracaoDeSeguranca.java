5ac3fb2009ace62f3ef02df659ec41acfd10e465
Vinícius <vinicius.raymundo@zup.com.br>
2022-01-04 11:28:18 -0300

    Tornar rotas post publicas para minha aplicacao

--- src/main/java/br/com/zup/LeadCollector/config/security/ConfiguracaoDeSeguranca.java
+++ src/main/java/br/com/zup/LeadCollector/config/security/ConfiguracaoDeSeguranca.java
@@ -1,11 +1,30 @@
 package br.com.zup.LeadCollector.config.security;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
 public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
+
+    private static final String[] ENDPOINT_POST_PUBLICO = {
+            "/leads",
+            "/usuario"
+    };
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.POST, ENDPOINT_POST_PUBLICO).permitAll()
+                .anyRequest().authenticated();
+    }
 
 }
