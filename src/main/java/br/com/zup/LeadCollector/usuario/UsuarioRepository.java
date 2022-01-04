393bf42746e703bb29edaa688a5d764a314db089
Vinícius <vinicius.raymundo@zup.com.br>
2022-01-04 14:57:43 -0300

    Criar metodo de buscar de usuario por email

--- src/main/java/br/com/zup/LeadCollector/usuario/UsuarioRepository.java
+++ src/main/java/br/com/zup/LeadCollector/usuario/UsuarioRepository.java
@@ -2,8 +2,11 @@ package br.com.zup.LeadCollector.usuario;
 
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.Optional;
 import java.util.UUID;
 
 public interface UsuarioRepository extends CrudRepository<Usuario, UUID> {
+
+    Optional<Usuario> findByEmail(String email);
 
 }
