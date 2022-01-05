ab98d5af38ac00384329e357f54397f493d6c16d
Vinícius <vinicius.raymundo@zup.com.br>
2022-01-05 15:44:01 -0300

    Implementar filtro de autenticacao via jwt

--- src/main/java/br/com/zup/LeadCollector/config/security/ConfiguracaoDeSeguranca.java
+++ src/main/java/br/com/zup/LeadCollector/config/security/ConfiguracaoDeSeguranca.java
@@ -1,12 +1,17 @@
 package br.com.zup.LeadCollector.config.security;
 
+import br.com.zup.LeadCollector.config.security.JWT.FiltroDeAutenticacaoJWT;
+import br.com.zup.LeadCollector.config.security.JWT.JWTComponent;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
@@ -15,6 +20,11 @@ import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 @Configuration
 @EnableWebSecurity
 public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private JWTComponent jwtComponent;
+    @Autowired
+    //Esse userDetailsService vai receber o objeto de UsuarioLoginService
+    private UserDetailsService detailsService;
 
     private static final String[] ENDPOINT_POST_PUBLICO = {
             "/leads",
@@ -31,6 +41,13 @@ public class ConfiguracaoDeSeguranca extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();
         //Session Policy Stateless não guardará estados de sessão dos usuarios.
         http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+
+        http.addFilter(new FiltroDeAutenticacaoJWT(jwtComponent, authenticationManager()));
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+       auth.userDetailsService(detailsService).passwordEncoder(bCryptPasswordEncoder());
     }
 
     @Bean
