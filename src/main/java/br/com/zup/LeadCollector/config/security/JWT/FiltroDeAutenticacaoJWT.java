af5b78115ef948cee002e6fcaaa466f733fd66b4
Vinícius <vinicius.raymundo@zup.com.br>
2022-01-05 15:10:56 -0300

    Sobrescrever attemptAuthentication

--- src/main/java/br/com/zup/LeadCollector/config/security/JWT/FiltroDeAutenticacaoJWT.java
+++ src/main/java/br/com/zup/LeadCollector/config/security/JWT/FiltroDeAutenticacaoJWT.java
@@ -1,8 +1,19 @@
 package br.com.zup.LeadCollector.config.security.JWT;
 
 
+import br.com.zup.LeadCollector.config.security.JWT.exceptions.AcessoNegadoException;
+import br.com.zup.LeadCollector.usuario.dtos.LoginDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.ArrayList;
 
 public class FiltroDeAutenticacaoJWT extends UsernamePasswordAuthenticationFilter {
     private JWTComponent jwtComponent;
@@ -13,5 +24,21 @@ public class FiltroDeAutenticacaoJWT extends UsernamePasswordAuthenticationFilte
         this.authenticationManager = authenticationManager;
     }
 
+    @Override
+    public Authentication attemptAuthentication(HttpServletRequest request, HttpServletResponse response) throws AuthenticationException {
+        ObjectMapper objectMapper = new ObjectMapper();
 
+        try{
+            LoginDTO login =  objectMapper.readValue(request.getInputStream(), LoginDTO.class);
+
+            UsernamePasswordAuthenticationToken authToken = new UsernamePasswordAuthenticationToken(
+                    login.getEmail(), login.getSenha(), new ArrayList<>()
+            );
+
+            Authentication autenticacao = authenticationManager.authenticate(authToken);
+            return autenticacao;
+        }catch (IOException e){
+            throw new AcessoNegadoException();
+        }
+    }
 }
---
+++ src/main/java/br/com/zup/LeadCollector/config/security/JWT/exceptions/AcessoNegadoException.java
@@ -0,0 +1,4 @@
+package br.com.zup.LeadCollector.config.security.JWT.exceptions;
+
+public class AcessoNegadoException extends RuntimeException{
+}
