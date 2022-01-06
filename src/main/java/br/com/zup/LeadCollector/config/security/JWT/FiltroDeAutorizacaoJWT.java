38cf4256a4832d072ae582d64dde267859a7c780
Vinícius <vinicius.raymundo@zup.com.br>
2022-01-06 11:36:49 -0300

    Sobrescrever doFilterInternal

--- src/main/java/br/com/zup/LeadCollector/config/security/JWT/FiltroDeAutorizacaoJWT.java
+++ src/main/java/br/com/zup/LeadCollector/config/security/JWT/FiltroDeAutorizacaoJWT.java
@@ -4,9 +4,16 @@ import br.com.zup.LeadCollector.config.security.JWT.exceptions.TokenInvalidoExce
 import io.jsonwebtoken.Claims;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
 public class FiltroDeAutorizacaoJWT extends BasicAuthenticationFilter {
     private JWTComponent jwtComponent;
@@ -28,6 +35,24 @@ public class FiltroDeAutorizacaoJWT extends BasicAuthenticationFilter {
         UserDetails usuarioLogado = userDetailsService.loadUserByUsername(claims.getSubject());
 
         return new UsernamePasswordAuthenticationToken(usuarioLogado, null, usuarioLogado.getAuthorities());
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain chain)
+            throws IOException, ServletException {
+
+        String token = request.getHeader("Authorization");
+
+        if (token != null && token.startsWith("Token ")) {
+            try {
+                UsernamePasswordAuthenticationToken auth = pegarAutenticacao(token.substring(6));
+                SecurityContextHolder.getContext().setAuthentication(auth);
+            } catch (TokenInvalidoException exception) {
+                System.out.println();
+            }
+        }
+
+        chain.doFilter(request, response);
     }
 
 }
