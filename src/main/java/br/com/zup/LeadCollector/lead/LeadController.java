994a266d76de9fd16780b1789402f5d317dfa3a8
Vinícius <vinicius.raymundo@zup.com.br>
2022-01-06 13:48:53 -0300

    Implementar Crud completo para lead

--- src/main/java/br/com/zup/LeadCollector/lead/LeadController.java
+++ src/main/java/br/com/zup/LeadCollector/lead/LeadController.java
@@ -4,21 +4,34 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
+import javax.validation.constraints.Email;
+
 @RestController
 @RequestMapping("/leads")
 public class LeadController {
     @Autowired
     private LeadService leadService;
 
-    @PostMapping()
+    @PostMapping()//C
     @ResponseStatus(HttpStatus.CREATED)
     public Lead cadastrarLead(@RequestBody Lead lead){
         return leadService.salvarLead(lead);
     }
 
-    @GetMapping
+    @GetMapping//R
     public Iterable<Lead> exibirTodosLeads(){
         return leadService.exibirLeads();
     }
 
+    @PutMapping//U
+    public Lead ataualizarLead(@RequestBody Lead lead){
+        return leadService.atualizarLead(lead);
+    }
+
+    @DeleteMapping//D
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deletarLead(@RequestParam String email){
+        leadService.deletarLead(email);
+    }
 }
--- src/main/java/br/com/zup/LeadCollector/lead/LeadService.java
+++ src/main/java/br/com/zup/LeadCollector/lead/LeadService.java
@@ -16,4 +16,11 @@ public class LeadService {
         return leadRepository.findAll();
     }
 
+    public Lead atualizarLead(Lead lead){
+        return leadRepository.save(lead);
+    }
+
+    public void deletarLead(String email){
+        leadRepository.deleteById(email);
+    }
 }
